587e864300f52f9f0fb92da266cd641f0426d37a
ianjam658 <james006ian@gmail.com>
2026-09-05 08:29:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,18 @@
 module.exports = {
   botname: "CYPHER-X",
-
   ownername: "Wafula",
-
-  // Kenya number in international format.
-  // Do NOT put +, spaces, or leading 0.
   owner: "254715068518",
+  prefix: ".",
 
-  prefix: "."
+  // --- NEW FEATURES 1-7 ---
+  autoViewStatus: true,
+  autoLikeStatus: true,
+  autoReactStatus: true,
+  statusReactEmoji: "❤️",
+  autoStatusSave: true,
+  antiDelete: true,
+  antiViewOnce: true,
+  antiCall: true,
+  welcome: true,
+  // ------------------------
 };
